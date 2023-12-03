403111f6ee7a102e46e9061b3f9443c75e64cfaf
toma <toma.watanabe@rakus-partners.co.jp>
2023-12-03 16:04:54 +0900

    コピー機能、オプションの活性制御

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,19 @@ export default function Home() {
 
       {/* コンテンツ部 */}
       <div className="">
-        <p>このサイトは入力した文字のアスキーアートを返すサイトです</p>
+        <p>
+          入力した文字をX（旧Twitter）用のアスキーアートにして返すサイトです。{" "}
+          <br />
+          ・漢字、ひらがなはカタカナに変換されます。
+          <br />
+          ・ひらがなは対応予定です。（のちのち...）
+          <br />
+          ・漢字は対応予定ないです。（無理なので...）
+          <br />
+          ・文字コードはUnicodeです。
+        </p>
         {/* 入力 */}
         <Form />
-        {/* チェックボックス・「ランダム作成する」 */}
-        {/* チェックボックス・ひらがな */}
-        {/* チェックボックス・カタカナ */}
-        {/* チェックボックス・アルファベット */}
-        {/* チェックボックス・記号 */}
-        {/* ボタン（作成） */}
-        {/* 結果 */}
       </div>
 
       {/* フッター部 */}
@@ -33,88 +36,5 @@ export default function Home() {
         />
       </div>
     </main>
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    //   <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-    //     <Image
-    //       className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Docs{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Find in-depth information about Next.js features and API.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Learn{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Learn about Next.js in an interactive course with&nbsp;quizzes!
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Templates{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Explore starter templates for Next.js.
-    //       </p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2 className={`mb-3 text-2xl font-semibold`}>
-    //         Deploy{' '}
-    //         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //           -&gt;
-    //         </span>
-    //       </h2>
-    //       <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
   );
 }
--- src/app/ui/form/form.tsx
+++ src/app/ui/form/form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChangeEvent, useRef, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
@@ -13,27 +14,142 @@ export default function Form() {
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("form")); // watch input value by passing the name of it
+  // 変換結果のState
+  const [result, setResult] = useState("");
+
+  // ランダム生成のチェックボックスのState
+  const [useRandom, setUseRandom] = useState(false);
+  // 変換オプション(ひらがな)を使用するかのState
+  const [useHiragana, setUseHiragana] = useState(false);
+  // 変換オプション(カタカナ)を使用するかのState
+  const [useKatakana, setUseKatakana] = useState(false);
+  // 変換オプション(アルファベット)を使用するかのState
+  const [useAlphabet, setUseAlphabet] = useState(false);
+  // 変換オプション(記号)を使用するかのState
+  const [useMark, setUseMark] = useState(false);
+
+  const formBRef = useRef();
+
+  // 変換ボタン押下
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    setResult(data.form);
+  };
+
+  // ランダム作成ボタン押下
+  const handleUseRandom = (e: ChangeEvent<HTMLInputElement>): void => {
+    // オプションチェックボックスの活性制御
+    setUseRandom(e.target.checked);
+
+    if (e.target.checked) {
+      // 各オプションをチェックする
+      setUseHiragana(e.target.checked);
+      setUseKatakana(e.target.checked);
+      setUseAlphabet(e.target.checked);
+      setUseMark(e.target.checked);
+    }
+  };
+
+  // コピーボタン押下
+  const handleCopy = async () => {
+    try {
+      // 結果をクリップボードに保存
+      await navigator.clipboard.writeText(result);
+      // 結果を全範囲選択
+      //   formBRef.current.select();
+      console.log("Copy Succeed");
+    } catch (err) {
+      console.log("Copy Failed");
+    }
+  };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input
-        {...register("form", {
-          required: "入力してください！",
-          maxLength: {
-            value: 4,
-            message: "4文字以内で入力してください",
-          },
-        })}
-      />
-      {/* errors will return when field validation fails  */}
-      {errors.form && <span>{errors.form.message}</span>}
-
-      <input type="submit" value="変換" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* include validation with required or other standard HTML validation rules */}
+        <input
+          {...register("form", {
+            required: "入力してください！",
+            maxLength: {
+              value: 4,
+              message: "4文字以内でお願いします（；；）",
+            },
+          })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.form && <span>{errors.form.message}</span>}
+        <div>
+          {/* チェックボックス・「ランダム作成する」 */}
+          <label>
+            <input type="checkbox" onChange={(e) => handleUseRandom(e)} />
+            ランダム生成
+          </label>
+          {/* チェックボックス・ひらがな */}
+          {/* <label>
+            <input
+              type="checkbox"
+              name="demoCheckbox"
+              checked={useHiragana}
+              onChange={(e) => setUseHiragana(e.target.checked)}
+              disabled={true}
+            />
+            ひらがな（未対応）
+          </label> */}
+          {/* チェックボックス・カタカナ */}
+          <label>
+            <input
+              type="checkbox"
+              name="demoCheckbox"
+              checked={useKatakana}
+              onChange={(e) => setUseKatakana(e.target.checked)}
+              disabled={!useRandom}
+            />
+            カタカナ
+          </label>
+          {/* チェックボックス・アルファベット */}
+          <label>
+            <input
+              type="checkbox"
+              name="demoCheckbox"
+              checked={useAlphabet}
+              onChange={(e) => setUseAlphabet(e.target.checked)}
+              disabled={!useRandom}
+            />
+            アルファベット
+          </label>
+          {/* チェックボックス・記号 */}
+          <label>
+            <input
+              type="checkbox"
+              name="demoCheckbox"
+              checked={useMark}
+              onChange={(e) => setUseMark(e.target.checked)}
+              disabled={!useRandom}
+            />
+            記号
+          </label>
+        </div>
+        {/* 変換 */}
+        <div>
+          <input type="submit" value="変換" />
+        </div>
+      </form>
+      {/* 結果 */}
+      <div>
+        <textarea
+          name=""
+          id=""
+          placeholder="結果"
+          ref={formBRef}
+          value={result}
+          onChange={(e) => setResult(e.target.value)}
+        ></textarea>
+      </div>
+      {/* 結果をコピー */}
+      <div>
+        <button onClick={handleCopy}>COPY</button>
+      </div>
+    </>
   );
 }
