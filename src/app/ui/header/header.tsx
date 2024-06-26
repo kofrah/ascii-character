adb41374df15d3d4d28c15931109016adc76d290
kofrah <kofrah20@gmail.com>
2024-06-26 23:50:13 +0900

    headerを画像に

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,25 +1,12 @@
 import styles from "@/app/ui/home.module.css";
+import Image from "next/image";
+import localImage from "../../../../public/header.png";
 
 export default function Header() {
   return (
     <>
       <div className={styles.title}>
-        <div>
-          <p>　┏━┏┳┓　┏┓　　　┏━━━━┓　　　┏┳┓</p>
-          <p>　┗━┗┻┛┏┛┗━━┓┗━┓┏━┛┏━━┗┻┫</p>
-          <p>┏━━━━┓┗┓┏━┓┃┏━┛┗━┓┣━━┫┃┃</p>
-          <p>┗━┓┏━┛　┃┃　┃┃┗━┓┏━┛┗━━┛┃┃</p>
-          <p>　┏┛┛　　┏┛┛┏┛┃　　┃┗━┓┏━━━┛┃</p>
-          <p>　┗┛　　　┗┛　┗━┛　　┗━━┛┗━━━━┛</p>
-        </div>
-        <div>
-          <p>　　　┏┳┓　　　　　　　┏┓　　┏┓　　　　　　　　　　┏━━━━┓</p>
-          <p>┏━━┗┻┫　　　　　┏━┛┗━┓┃┃　　　　　　　　　　┃┏━━┓┃</p>
-          <p>┣━━┫┃┃　┏━━┓┗━━┓┏┛┃┃　　┏┓┏━━━━┓┃┃┏━┛┃</p>
-          <p>┗━━┛┃┃　┗┓┏┛　┏┛┏╋┓┃┃　┏┏┛┗━━━━┛┗┛┗━┓┃</p>
-          <p>┏━━━┛┃　┏┛┗┓┏┛┫┃┗┛┃┗━┏┛　　　　　　　　　　┏┛┃</p>
-          <p>┗━━━━┛　┗━━┛┗┛┗┛　　┗━━┛　　　　　　　　　　　┗━┛</p>
-        </div>
+        <Image src={localImage} alt="header" width={500} height={300} />
       </div>
     </>
   );
