01e76b2bf50acdb91d9506ad06c8292c36d97ee2
kofrah <kofrah20@gmail.com>
2024-08-24 19:44:12 +0900

    text-base

--- src/app/ui/form/form.tsx
+++ src/app/ui/form/form.tsx
@@ -183,7 +183,12 @@ export default function Form() {
         <div>
           <input
             style={{ width: "100%" }}
-            className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            className="py-2.5 px-5 
+            me-2 mb-2 
+            text-base font-medium text-gray-900 
+            focus:outline-none bg-white rounded-lg border border-gray-200
+             hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200
+             dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             placeholder="ここに文字を入力"
             {...register("inputArea", {
               required: "入力してください！",
@@ -229,6 +234,7 @@ export default function Form() {
               type="button"
               className="py-2.5 px-5 me-2 w-32 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 
               hover:bg-gray-100 hover:text-blue-700
+              focus:z-10 focus:ring-4 focus:ring-gray-200 
               dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               value={button.value}
               onClick={handleClickCreateButton}
