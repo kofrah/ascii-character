b43c2e4e68c37bea5e24edee383e849bed98ec4f
kofrah <kofrah20@gmail.com>
2024-10-27 01:19:08 +0900

    fix metadata obj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,15 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title: string = "デカ文字ジェネレータ";
+const description: string =
+  "カタカナ、英数字、記号をデカいアスキーアートに変換できます。";
+
 // https://nextjs.org/docs/14/app/building-your-application/optimizing/metadata
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
 export const metadata: Metadata = {
-  title: "デカ文字ジェネレータ",
-  description: "カタカナ、英数字、記号をデカいアスキーアートに変換できます。",
+  title: title,
+  description: description,
   metadataBase: new URL("https://dekamoji-generator.com/"),
   alternates: {
     canonical: "/",
@@ -19,13 +23,15 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
+    title: title,
+    description: description,
     images: "/opengraph-image.png",
   },
   twitter: {
     card: "summary_large_image",
-    title: "デカ文字ジェネレータ",
-    description: "カタカナ、英数字、記号をデカいアスキーアートに変換できます。",
-    images: ["https://dekamoji-generator.com/opengraph-image.png"], // Must be an absolute URL
+    title: title,
+    description: description,
+    images: ["https://dekamoji-generator.com/twitter-image.png"], // Must be an absolute URL
   },
 };
 
