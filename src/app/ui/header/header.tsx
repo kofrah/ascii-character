9e5e18b3ae1192b61d63c9b4f08414bb3c65bf2a
kofrah <kofrah20@gmail.com>
2024-10-27 16:52:29 +0900

    remove unused meta from Header

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -5,10 +5,6 @@ import localImage from "../../../../public/header.png";
 export default function Header() {
   return (
     <>
-      <meta
-        name="twitter:image"
-        content="https://dekamoji-generator.com/favicon.ico"
-      />
       <div className={styles.title}>
         <Image src={localImage} alt="header" width={500} height={300} />
       </div>
