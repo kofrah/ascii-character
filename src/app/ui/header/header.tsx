b1ee3eca18774f99186a7e725caf5762157d4e62
kofrah <kofrah20@gmail.com>
2024-10-30 22:09:38 +0900

    Fix Header for darkmode

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,18 +1,46 @@
-import styles from "@/app/ui/home.module.css";
+"use client";
+
 import Image from "next/image";
-import localImage from "../../../../public/header.png";
+import headerForLightMode from "../../../../public/headerForLightMode.png";
+import headerForDarkMode from "../../../../public/headerForDarkMode.png";
+import { useEffect, useState } from "react";
+
+const LightModeComponent = () => (
+  <Image
+    src={headerForLightMode}
+    alt="デカ文字ジェネレータ"
+    width={500}
+    height={300}
+  />
+);
+
+const DarkModeComponent = () => (
+  <Image
+    src={headerForDarkMode}
+    alt="デカ文字ジェネレータ"
+    width={500}
+    height={300}
+  />
+);
 
 export default function Header() {
-  return (
-    <>
-      <div className={styles.title}>
-        <Image
-          src={localImage}
-          alt="デカ文字ジェネレータ"
-          width={500}
-          height={300}
-        />
-      </div>
-    </>
-  );
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // ダークモードかどうかを確認
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    console.log("mediaQuery", mediaQuery);
+    setIsDarkMode(mediaQuery.matches);
+
+    // ダークモードの切り替えをリッスン
+    const handleChange: (event: MediaQueryListEvent) => void = (event) => {
+      setIsDarkMode(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleChange);
+
+    // クリーンアップ
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return isDarkMode ? <DarkModeComponent /> : <LightModeComponent />;
 }
